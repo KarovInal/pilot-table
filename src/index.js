664c2b276f66b518450904061f703413fad97877
jamikk <jkarimov1593@gmail.com>
2017-10-03 15:39:16 +0300

    table done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Grid } from 'react-virtualized';
+import data from './data.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Grid data as an array of arrays
+const list = data;
+
+function cellRenderer ({ columnIndex, key, rowIndex, style }) {
+  return (
+    <div
+      key={key}
+      style={style}
+    >
+      {list[rowIndex][columnIndex].value}
+    </div>
+  )  
+}
+
+// Render your grid
+ReactDOM.render(
+  <Grid
+    cellRenderer={cellRenderer}
+    columnCount={list[0].length}
+    columnWidth={100}
+    height={600}
+    rowCount={list.length}
+    rowHeight={30}
+    width={1500}
+  />,
+  document.getElementById('root')
+);
