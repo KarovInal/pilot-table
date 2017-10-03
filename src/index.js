1cac0063592e0a2430b479cf972e5fcbfe776eb6
Inal Karov <bagov.ali07@gmail.com>
2017-10-03 14:37:57 +0300

    first compile

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Grid } from 'react-virtualized';
+import list from './table.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+console.log(list[0]);
+
+function cellRenderer ({ columnIndex, key, rowIndex, style }) {
+  return (
+    <div
+      key={key}
+      style={style}
+    >
+      {
+        list[rowIndex][columnIndex].value
+      }
+    </div>
+  )  
+}
+
+render(
+  <Grid
+    cellRenderer={cellRenderer}
+    columnCount={list.length}
+    columnWidth={100}
+    height={300}
+    rowCount={list.length}
+    rowHeight={30}
+    width={900}
+  />,
+  document.getElementById('root')
+)
