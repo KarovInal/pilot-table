bd06d4b02f89ec2e1a145e7647c54c4c4976d612
Artur Chernysh <Artur@achernysh.tsc.ts>
2017-10-04 12:55:20 +0300

    'editable-table'

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import { Grid } from 'react-virtualized';
 import list from './fakeJSON.json';
 
+const onCellMouseDown = (e) => {
+  if (e.target.getAttribute('data-editable') !== 'true') e.preventDefault();
+};
+
 const onCellClick = (e) => {
   if (e.target.getAttribute('data-editable') === 'true') e.target.setAttribute('contenteditable', true);
 };
@@ -17,6 +21,7 @@ function cellRenderer({ columnIndex, key, rowIndex, style }) {
          style={style}
          data-editable={ editable }
          data-type={ type }
+         onMouseDown={ onCellMouseDown }
          onClick={ onCellClick }>
       {
         list[rowIndex][columnIndex].value
